a8d031ab34305e4fb4a8ce366a3d2319f1d3ff04
bbloch <b9bloch@yahoo.fr>
2019-05-14 15:59:10 +0200

    Initialization + Clean folders🤘

---
+++ DeezerExercice/Sources/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  DeezerExercice
+//
+//  Created by Bertrand BLOC'H on 14/05/2019.
+//  Copyright © 2019 bblch. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
