6048e58af55d8d7d28e7243f92f193037d4108d2
bbloch <b9bloch@yahoo.fr>
2019-05-14 18:06:39 +0200

    Put UI Basis

---
+++ DeezerExercice/Sources/ArtistDetailsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ArtistDetailsViewController.swift
+//  DeezerExercice
+//
+//  Created by Bertrand BLOC'H on 14/05/2019.
+//  Copyright © 2019 bblch. All rights reserved.
+//
+
+import UIKit
+
+final class ArtistDetailsViewController: UIViewController {
+    
+}
---
+++ DeezerExercice/Sources/ArtistSearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ArtistSearchViewController.swift
+//  DeezerExercice
+//
+//  Created by Bertrand BLOC'H on 14/05/2019.
+//  Copyright © 2019 bblch. All rights reserved.
+//
+
+import UIKit
+
+final class ArtistSearchViewController: UIViewController {
+    
+}
